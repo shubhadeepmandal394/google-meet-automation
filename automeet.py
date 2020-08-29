b04210b585ce8f39f5834bb19b11632be1c018a6
Shubhadeep Mandal <shubhadeepmandal394@gmail.com>
2020-08-29 23:32:06 +0530

    Second Commit

--- automeet.py
+++ automeet.py
@@ -1,26 +1,22 @@
+# An Automation script to automatically join a scheduled google-meet meeting at a specific time without manual labour.
+
+# importing the required python packages
 import pyautogui
 import webbrowser
 import time
 from datetime import datetime
 
 while True:
-    now = datetime.now()
-    if now.strftime("%H%M") == "2120":
-        webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open_new("https://meet.google.com/pnz-hyap-pxb")
+    now = datetime.now()  # check the current system time
 
-        time.sleep(1)
-        pyautogui.moveTo(1750, 148)
-        pyautogui.click()
+    if now.strftime("%H%M%S") == "232430":  # check if the current system time matches your meeting time
+        webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open_new(
+            "https://meet.google.com/ssu-yfjs-vzd")  # accessing the web browser and prompting it to navigate you to the mentioned URL
 
-        time.sleep(2)
-        pyautogui.moveTo(909, 694, )
-        pyautogui.click()
+        time.sleep(5)  # making a delay of 5 seconds
+        pyautogui.hotkey('ctrl', 'd')  # Press the Ctrl-D hotkey combination.
+        pyautogui.hotkey('ctrl', 'e')  # Press the Ctrl-E hotkey combination.
 
-        time.sleep(5)
-        pyautogui.hotkey('ctrl', 'd')
-        pyautogui.hotkey('ctrl', 'e')
-
-        time.sleep(3)
-        pyautogui.moveTo(1425, 582)
-        pyautogui.click()
+        time.sleep(3)  # making a delay of 3 seconds
+        pyautogui.click(1425, 582)  # Move the mouse to XY coordinates and click it.
         break
